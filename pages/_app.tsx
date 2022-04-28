cddbccb451ce7d589111d2c01122428e36c7df8e
Sakpat Sirikanerat <fair.sakpat@gmail.com>
2022-04-28 23:48:26 +0700

    Added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,22 @@ import "react-loading-skeleton/dist/skeleton.css";
 import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
 import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider
-      serverUrl="https://yp9v1zktox9g.usemoralis.com:2053/server"
-      appId="UqiOmRO5mtuqqpaFfekoqHZe4FSizkJniQwS1Dvt"
-    >
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <>
+      <NextNProgress color="black" />
+      <MoralisProvider
+        serverUrl="https://yp9v1zktox9g.usemoralis.com:2053/server"
+        appId="UqiOmRO5mtuqqpaFfekoqHZe4FSizkJniQwS1Dvt"
+      >
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
   );
 }
 
